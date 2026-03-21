17d8428aca769ab377f3eb9cf2adb3d0ad8f79f8
Sagar Nadiya <87667750+nadiyasagar@users.noreply.github.com>
2026-03-21 17:19:38 +0530

    Delete app directory

--- app/src/main/java/com/example/composepod/demo/data/local/NotesDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.composepod.demo.data.local
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
-abstract class NotesDatabase : RoomDatabase() {
-    abstract fun noteDao(): NoteDao
-}
--- app/src/main/java/com/example/composepod/demo/models/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.example.composepod.demo.models
-
-data class Note(
-    val id: String,
-    val title: String,
-    val content: String,
-    val timestamp: Long = System.currentTimeMillis()
-)
